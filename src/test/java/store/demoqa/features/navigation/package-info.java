b48a5083cd2f01386145f6574838b1770ed58663
FlavianDurigu <flavi2k@gmail.com>
2017-04-06 11:29:20 +0300

    Narrative added to navigation

---
+++ src/test/java/store/demoqa/features/navigation/package-info.java
@@ -0,0 +1,7 @@
+@Narrative(
+        title = "Navigate to products",
+        text = {"Verify that all product categories are accessible to the user"}
+          )
+package store.demoqa.features.navigation;
+
+import net.thucydides.core.annotations.Narrative;
