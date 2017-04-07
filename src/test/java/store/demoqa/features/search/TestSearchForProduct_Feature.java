b719b585dc77f3a5b92368a76d839f3ab2b601c9
FlavianDurigu <flavi2k@gmail.com>
2017-04-07 16:48:15 +0300

    Added Search for Product

---
+++ src/test/java/store/demoqa/features/search/TestSearchForProduct_Feature.java
@@ -0,0 +1,35 @@
+package store.demoqa.features.search;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import store.demoqa.steps.SearchForProduct_Step;
+
+/**
+ * Created by FlavianDurigu on 4/6/2017.
+ */
+@RunWith(SerenityRunner.class)
+public class TestSearchForProduct_Feature {
+
+    @Steps
+    SearchForProduct_Step user1;
+
+    @Managed(driver="chrome")
+    WebDriver browser;
+
+    @Test
+    public void userShouldBeAbleToSearchForProducts() {
+        // Given
+        user1.isOnTheHomePage();
+
+        // When
+        user1.searchForProduct("iphone");
+
+        //Then
+        user1.shouldSeeProduct("iphone | Search Results  | ONLINE STORE");
+    }
+
+}
